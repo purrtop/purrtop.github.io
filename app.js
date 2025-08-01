6571327577f8eb2105085ce6d100f425c1a74b63
purrtop <153719946+purrtop@users.noreply.github.com>
2025-08-01 23:18:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
-var playBtn = document.getElementById('play'),
-  resetBtn = document.getElementById('reset'),
-  hearbeat = document.getElementById('heartbeat')
-	audios = document.querySelectorAll('audio');
-console.log(audios);
+var audio = document.getElementById("audio");
+var img = document.getElementById("button");
 
+function play() {
+	if (audio) {
+		audio.play();
+	}
+}
 
-playBtn.addEventListener('mouseover', function() {
-[].forEach.call(audios, function(audio) {
-  // do whatever
-  audio.play();
-});
-}, false);
-
-playBtn.addEventListener('mouseleave', function() {
-  hearbeat.pause();
-  hearbeat.currentTime = 0;
-}, false);
-
-resetBtn.addEventListener('mouseover', function() {
-    hearbeat.play();
-}, false);
-
-resetBtn.addEventListener('mouseleave', function() {
-  hearbeat.pause();
-  hearbeat.currentTime = 0;
-}, false);
+if (img && audio) {
+	img.addEventListener('click', play);
+	img.addEventListener('mouseover', play);
+}
