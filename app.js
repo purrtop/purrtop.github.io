77ea15888152a3d05b0c885ad85777679abf5736
purrtop <153719946+purrtop@users.noreply.github.com>
2025-08-02 12:09:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-var audio = document.getElementById("audio");
-var img = document.getElementById("button");
+var audio1 = document.getElementById("audio");
+var button1 = document.getElementById("button");
 
-function play() {
-	if (audio) {
-		audio.play();
+function playAudio1() {
+	if (audio1) {
+		audio1.play();
 	}
 }
 
-if (img && audio) {
-	img.addEventListener('click', play);
-	img.addEventListener('mouseover', play);
+if (button1 && audio1) {
+	button1.addEventListener('mouseover', playAudio1);
+}
+
+var audio2 = document.getElementById("audio2");
+var button2 = document.getElementById("button");
+
+function playAudio2() {
+	if (audio2) {
+		audio2.play();
+	}
+}
+
+if (button2 && audio2) {
+	button2.addEventListener('mouseout', playAudio2);
 }
